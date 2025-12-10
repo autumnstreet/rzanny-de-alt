d5f2e85bb5ec167a566b1266cdd04e7982539570
Sascha <sascha@autumnstreet.de>
2025-12-11 00:17:39 +0100

    refactor: replace deprecated object elements with audio tags for better compatibility

--- Rzanny.de/Audio_Video_files/Audio_Video.js
+++ Rzanny.de/Audio_Video_files/Audio_Video.js
@@ -1,54 +1,19 @@
 // Created by iWeb 3.0.4 local-build-20150910
 
 function writeMovie1()
-{detectBrowser();if(windowsInternetExplorer)
-{document.write('<object id="id2" classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B" codebase="http://www.apple.com/qtactivex/qtplugin.cab" width="230" height="16" style="height: 16px; left: 468px; position: absolute; top: 151px; width: 230px; z-index: 1; "><param name="src" value="Media/03%20Titel%2003.m4a" /><param name="controller" value="true" /><param name="autoplay" value="false" /><param name="scale" value="tofit" /><param name="volume" value="100" /><param name="loop" value="false" /></object>');}
-else if(isiPhone)
-{document.write('<object id="id2" type="video/quicktime" width="230" height="16" style="height: 16px; left: 468px; position: absolute; top: 151px; width: 230px; z-index: 1; "><param name="src" value="Media/03%20Titel%2003.m4a"/><param name="controller" value="true"/><param name="scale" value="tofit"/></object>');}
-else
-{document.write('<object id="id2" type="video/quicktime" width="230" height="16" data="Media/03%20Titel%2003.m4a" style="height: 16px; left: 468px; position: absolute; top: 151px; width: 230px; z-index: 1; "><param name="src" value="Media/03%20Titel%2003.m4a"/><param name="controller" value="true"/><param name="autoplay" value="false"/><param name="scale" value="tofit"/><param name="volume" value="100"/><param name="loop" value="false"/></object>');}}
+{document.write('<audio id="id2" controls style="height: 32px; left: 468px; position: absolute; top: 138px; width: 230px; z-index: 1;"><source src="Media/03%20Titel%2003.m4a" type="audio/mp4">Ihr Browser unterstützt das Audio-Element nicht.</audio>');}
 function writeMovie2()
-{detectBrowser();if(windowsInternetExplorer)
-{document.write('<object id="id5" classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B" codebase="http://www.apple.com/qtactivex/qtplugin.cab" width="230" height="16" style="height: 16px; left: 468px; position: absolute; top: 261px; width: 230px; z-index: 1; "><param name="src" value="Media/BusyLines.mp3" /><param name="controller" value="true" /><param name="autoplay" value="false" /><param name="scale" value="tofit" /><param name="volume" value="100" /><param name="loop" value="false" /></object>');}
-else if(isiPhone)
-{document.write('<object id="id5" type="video/quicktime" width="230" height="16" style="height: 16px; left: 468px; position: absolute; top: 261px; width: 230px; z-index: 1; "><param name="src" value="Media/BusyLines.mp3"/><param name="controller" value="true"/><param name="scale" value="tofit"/></object>');}
-else
-{document.write('<object id="id5" type="video/quicktime" width="230" height="16" data="Media/BusyLines.mp3" style="height: 16px; left: 468px; position: absolute; top: 261px; width: 230px; z-index: 1; "><param name="src" value="Media/BusyLines.mp3"/><param name="controller" value="true"/><param name="autoplay" value="false"/><param name="scale" value="tofit"/><param name="volume" value="100"/><param name="loop" value="false"/></object>');}}
+{document.write('<audio id="id5" controls style="height: 32px; left: 468px; position: absolute; top: 250px; width: 230px; z-index: 1;"><source src="Media/BusyLines.mp3" type="audio/mpeg">Ihr Browser unterstützt das Audio-Element nicht.</audio>');}
 function writeMovie3()
-{detectBrowser();if(windowsInternetExplorer)
-{document.write('<object id="id6" classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B" codebase="http://www.apple.com/qtactivex/qtplugin.cab" width="230" height="16" style="height: 16px; left: 468px; position: absolute; top: 314px; width: 230px; z-index: 1; "><param name="src" value="Media/AJ%27sFitness.mp3" /><param name="controller" value="true" /><param name="autoplay" value="false" /><param name="scale" value="tofit" /><param name="volume" value="100" /><param name="loop" value="false" /></object>');}
-else if(isiPhone)
-{document.write('<object id="id6" type="video/quicktime" width="230" height="16" style="height: 16px; left: 468px; position: absolute; top: 314px; width: 230px; z-index: 1; "><param name="src" value="Media/AJ%27sFitness.mp3"/><param name="controller" value="true"/><param name="scale" value="tofit"/></object>');}
-else
-{document.write('<object id="id6" type="video/quicktime" width="230" height="16" data="Media/AJ%27sFitness.mp3" style="height: 16px; left: 468px; position: absolute; top: 314px; width: 230px; z-index: 1; "><param name="src" value="Media/AJ%27sFitness.mp3"/><param name="controller" value="true"/><param name="autoplay" value="false"/><param name="scale" value="tofit"/><param name="volume" value="100"/><param name="loop" value="false"/></object>');}}
+{document.write('<audio id="id6" controls style="height: 32px; left: 468px; position: absolute; top: 302px; width: 230px; z-index: 1;"><source src="Media/AJ%27sFitness.mp3" type="audio/mpeg">Ihr Browser unterstützt das Audio-Element nicht.</audio>');}
 function writeMovie4()
-{detectBrowser();if(windowsInternetExplorer)
-{document.write('<object id="id8" classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B" codebase="http://www.apple.com/qtactivex/qtplugin.cab" width="230" height="16" style="height: 16px; left: 468px; position: absolute; top: 365px; width: 230px; z-index: 1; "><param name="src" value="Media/Take%2001.mp3" /><param name="controller" value="true" /><param name="autoplay" value="false" /><param name="scale" value="tofit" /><param name="volume" value="100" /><param name="loop" value="false" /></object>');}
-else if(isiPhone)
-{document.write('<object id="id8" type="video/quicktime" width="230" height="16" style="height: 16px; left: 468px; position: absolute; top: 365px; width: 230px; z-index: 1; "><param name="src" value="Media/Take%2001.mp3"/><param name="controller" value="true"/><param name="scale" value="tofit"/></object>');}
-else
-{document.write('<object id="id8" type="video/quicktime" width="230" height="16" data="Media/Take%2001.mp3" style="height: 16px; left: 468px; position: absolute; top: 365px; width: 230px; z-index: 1; "><param name="src" value="Media/Take%2001.mp3"/><param name="controller" value="true"/><param name="autoplay" value="false"/><param name="scale" value="tofit"/><param name="volume" value="100"/><param name="loop" value="false"/></object>');}}
+{document.write('<audio id="id8" controls style="height: 32px; left: 468px; position: absolute; top: 358px; width: 230px; z-index: 1;"><source src="Media/Take%2001.mp3" type="audio/mpeg">Ihr Browser unterstützt das Audio-Element nicht.</audio>');}
 function writeMovie5()
-{detectBrowser();if(windowsInternetExplorer)
-{document.write('<object id="id10" classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B" codebase="http://www.apple.com/qtactivex/qtplugin.cab" width="230" height="16" style="height: 16px; left: 468px; position: absolute; top: 417px; width: 230px; z-index: 1; "><param name="src" value="Media/Sommerkatalog.mp3" /><param name="controller" value="true" /><param name="autoplay" value="false" /><param name="scale" value="tofit" /><param name="volume" value="100" /><param name="loop" value="false" /></object>');}
-else if(isiPhone)
-{document.write('<object id="id10" type="video/quicktime" width="230" height="16" style="height: 16px; left: 468px; position: absolute; top: 417px; width: 230px; z-index: 1; "><param name="src" value="Media/Sommerkatalog.mp3"/><param name="controller" value="true"/><param name="scale" value="tofit"/></object>');}
-else
-{document.write('<object id="id10" type="video/quicktime" width="230" height="16" data="Media/Sommerkatalog.mp3" style="height: 16px; left: 468px; position: absolute; top: 417px; width: 230px; z-index: 1; "><param name="src" value="Media/Sommerkatalog.mp3"/><param name="controller" value="true"/><param name="autoplay" value="false"/><param name="scale" value="tofit"/><param name="volume" value="100"/><param name="loop" value="false"/></object>');}}
+{document.write('<audio id="id10" controls style="height: 32px; left: 468px; position: absolute; top: 410px; width: 230px; z-index: 1;"><source src="Media/Sommerkatalog.mp3" type="audio/mpeg">Ihr Browser unterstützt das Audio-Element nicht.</audio>');}
 function writeMovie6()
-{detectBrowser();if(windowsInternetExplorer)
-{document.write('<object id="id16" classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B" codebase="http://www.apple.com/qtactivex/qtplugin.cab" width="230" height="16" style="height: 16px; left: 468px; position: absolute; top: 484px; width: 230px; z-index: 1; "><param name="src" value="Media/Ines%20Demo_Bareso.mp3" /><param name="controller" value="true" /><param name="autoplay" value="false" /><param name="scale" value="tofit" /><param name="volume" value="100" /><param name="loop" value="false" /></object>');}
-else if(isiPhone)
-{document.write('<object id="id16" type="video/quicktime" width="230" height="16" style="height: 16px; left: 468px; position: absolute; top: 484px; width: 230px; z-index: 1; "><param name="src" value="Media/Ines%20Demo_Bareso.mp3"/><param name="controller" value="true"/><param name="scale" value="tofit"/></object>');}
-else
-{document.write('<object id="id16" type="video/quicktime" width="230" height="16" data="Media/Ines%20Demo_Bareso.mp3" style="height: 16px; left: 468px; position: absolute; top: 484px; width: 230px; z-index: 1; "><param name="src" value="Media/Ines%20Demo_Bareso.mp3"/><param name="controller" value="true"/><param name="autoplay" value="false"/><param name="scale" value="tofit"/><param name="volume" value="100"/><param name="loop" value="false"/></object>');}}
+{document.write('<audio id="id16" controls style="height: 32px; left: 468px; position: absolute; top: 478px; width: 230px; z-index: 1;"><source src="Media/Ines%20Demo_Bareso.mp3" type="audio/mpeg">Ihr Browser unterstützt das Audio-Element nicht.</audio>');}
 function writeMovie7()
-{detectBrowser();if(windowsInternetExplorer)
-{document.write('<object id="id18" classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B" codebase="http://www.apple.com/qtactivex/qtplugin.cab" width="230" height="16" style="height: 16px; left: 468px; position: absolute; top: 537px; width: 230px; z-index: 1; "><param name="src" value="Media/Ines%20Demo_Netcom.mp3" /><param name="controller" value="true" /><param name="autoplay" value="false" /><param name="scale" value="tofit" /><param name="volume" value="100" /><param name="loop" value="false" /></object>');}
-else if(isiPhone)
-{document.write('<object id="id18" type="video/quicktime" width="230" height="16" style="height: 16px; left: 468px; position: absolute; top: 537px; width: 230px; z-index: 1; "><param name="src" value="Media/Ines%20Demo_Netcom.mp3"/><param name="controller" value="true"/><param name="scale" value="tofit"/></object>');}
-else
-{document.write('<object id="id18" type="video/quicktime" width="230" height="16" data="Media/Ines%20Demo_Netcom.mp3" style="height: 16px; left: 468px; position: absolute; top: 537px; width: 230px; z-index: 1; "><param name="src" value="Media/Ines%20Demo_Netcom.mp3"/><param name="controller" value="true"/><param name="autoplay" value="false"/><param name="scale" value="tofit"/><param name="volume" value="100"/><param name="loop" value="false"/></object>');}}
+{document.write('<audio id="id18" controls style="height: 32px; left: 468px; position: absolute; top: 530px; width: 230px; z-index: 1;"><source src="Media/Ines%20Demo_Netcom.mp3" type="audio/mpeg">Ihr Browser unterstützt das Audio-Element nicht.</audio>');}
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
 function hostedOnDM()
